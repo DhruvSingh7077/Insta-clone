bf3b6465c94d7ed81560bde2bfb01953e089f3c1
Dhruv Singh <dhruvs.22.becs@acharya.ac.in>
2025-08-24 20:34:50 +0530

    Add files via upload

---
+++ SavedPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const SavedPage = () => {
+  return (
+    <div className="text-white p-4">
+      <h2>Saved Posts</h2>
+      <p>All your saved posts will appear here.</p>
+    </div>
+  );
+};
+
+export default SavedPage;
