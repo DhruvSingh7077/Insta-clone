d4950a964560cb8ee6d28e63b082458a00156249
Dhruv Singh <dhruvs.22.becs@acharya.ac.in>
2025-08-26 21:05:15 +0530

    Add files via upload

--- CreatePost.js
+++ CreatePost.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useState } from "react";
+import "./CreatePost.css";
 
-const CreatePost = () => {
+const CreatePost = ({ showModal, setShowModal }) => {
   return (
-    <div className="p-4">
-      <h2>Create Post</h2>
-      <p>This is the Create Post page. Here you can upload images, write captions, etc.</p>
-    </div>
+    <>
+      {/* Modal */}
+      {showModal && (
+        <div className="modal-overlay">
+          <div className="modal-container">
+            <div className="modal-header">
+              <h5>Create new post</h5>
+              <button
+                className="btn-close"
+                onClick={() => setShowModal(false)}
+              />
+            </div>
+            <div className="modal-body text-center">
+              <div className="upload-box">
+                <i className="bi bi-images" style={{ fontSize: "3rem" }}></i>
+                <p>Drag photos and videos here</p>
+                <button className="btn btn-primary">
+                  Select from computer
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
